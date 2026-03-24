b9c5918b760112c95a0d75c5f68be769c7896473
Evgenii Timofeev <timofeev.e.v.90@gmail.com>
2026-03-24 20:45:01 +0300

    Add migration for sso_enabled column on plans table

---
+++ datanika/migrations/versions/p5l2m3n4o6i7_add_sso_enabled_to_plans.py
@@ -0,0 +1,30 @@
+"""Add sso_enabled column to plans table
+
+Revision ID: p5l2m3n4o6i7
+Revises: o4k1l2m3n5h6
+Create Date: 2026-03-24 20:00:00.000000
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+revision: str = "p5l2m3n4o6i7"
+down_revision: Union[str, None] = "o4k1l2m3n5h6"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "plans",
+        sa.Column("sso_enabled", sa.Boolean(), nullable=False, server_default=sa.text("false")),
+    )
+    op.execute(
+        "UPDATE plans SET sso_enabled = true WHERE slug = 'enterprise-monthly'"
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("plans", "sso_enabled")
